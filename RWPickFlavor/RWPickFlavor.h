bdbb1ab1971ac5cde1a9c4cf39ab7b06528b9db9
Nadya Karaban <nadya@gini.net>
2020-12-01 13:07:58 +0100

    Initial Commit

---
+++ RWPickFlavor/RWPickFlavor.h
@@ -0,0 +1,18 @@
+//
+//  RWPickFlavor.h
+//  RWPickFlavor
+//
+//  Created by Nadya Karaban on 01.12.20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RWPickFlavor.
+FOUNDATION_EXPORT double RWPickFlavorVersionNumber;
+
+//! Project version string for RWPickFlavor.
+FOUNDATION_EXPORT const unsigned char RWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavor/PublicHeader.h>
+
+
